7ec6665a0ac16f4ab29612ce237fc0a86ed129c8
Castlebin <Castlebin@gmail.com>
2025-05-13 09:19:40 +0800

    <!-- spring boot 项目 绝大多数情况下直接使用 这种方式是最佳的 -->     <!-- pom parent 用 spring-boot-starter-parent -->

---
+++ ROOT_PROJECT_spring-boot-parent/SUB_1_spring_boot/src/test/java/com/heller/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heller.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
